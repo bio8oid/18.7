d7f4579900fbd886d2e22cbb933cf0a62cb0d3d6
bio8oid <8bollod8@mail.co.uk>
2019-06-23 22:52:09 +0200

    upade image2

--- components/Contact.js
+++ components/Contact.js
@@ -7,8 +7,7 @@ var Contact = React.createClass({
     return (
         <div className = {'contactItem'}>
           
-        
-          <img className = {'contactImage'} src = {'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwj_5PaesYDjAhVJxKYKHdqxCksQjRx6BAgBEAU&url=https%3A%2F%2Fwww.gettyimages.com%2Fphotos%2Fthe-muppets&psig=AOvVaw1Cm4Eb8y1av2npgSiLmb2_&ust=1561406128886851'} />
+          <img className = {'contactImage'} src = {'./images/ico.png'} />
           <p className = {'contactLabel'}>
             Imię: {this.props.item.firstName}
           </p>
