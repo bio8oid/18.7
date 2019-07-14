f94eba9cf1f61d413dd6d3c5051605411a1faf34
bio8oid <47393762+bio8oid@users.noreply.github.com>
2019-07-15 00:17:41 +0200

    Update Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,21 +1,21 @@
 var contacts = [
   {
     id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com'
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com'
   },
   {
     id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com'
+    firstName: 'Mark',
+    lastName: 'Smith',
+    email: 'mark.smith@example.com'
   },
   {
     id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com'
+    firstName: 'Jack',
+    lastName: 'Turner',
+    email: 'jack.turner@example.com'
   }
 ];
 
